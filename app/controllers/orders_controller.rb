fd2dcc75107d95b1bd1f0f6e9909b499d44187c5
cwarlen-dice <sevens_lovers@yahoo.co.jp>
2021-05-20 14:23:41 +0900

    OrderDestinationフォームオブジェクト：バリデーション設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,21 +1,17 @@
 class OrdersController < ApplicationController
-  def index
-    @item = Item.find(params[:item_id])
-    @order_destination = OrderDestination.new
-  end
+  before_action :one_item_set, only: %i[index create]
 
-  def new
-    # @order_destination = OrderDestination.new
+  def index
+    @order_destination = OrderDestination.new
   end
 
   def create
     @order_destination = OrderDestination.new(order_params)
-    binding.pry
     if @order_destination.valid?
       @order_destination.save
-      redirect_to root_path
+      redirect_to(item_path)
     else
-      render :new
+      render :index
     end
   end
 
@@ -24,5 +20,9 @@ class OrdersController < ApplicationController
   def order_params
     params.require(:order_destination).permit(:postal_code, :prefecture_id, :city, :addresses, :building,
                                               :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
+
+  def one_item_set
+    @item = Item.find(params[:item_id])
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,5 @@ class Order < ApplicationRecord
   belongs_to :user
   belongs_to :item
   has_one :destination
+  belongs_to :image, through: :item
 end
