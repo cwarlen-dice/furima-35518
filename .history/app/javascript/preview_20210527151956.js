b4876552f08e5eafbe605738d8db0061fe85b775
cwarlen-dice <sevens_lovers@yahoo.co.jp>
2021-05-27 15:50:41 +0900

    画像プレビュー

---
+++ .history/app/javascript/preview_20210527151956.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', function () {
+  if (document.getElementById('message_image')) { // 変化監視要素
+    const ImageList = document.getElementById('image-list'); // プレビュー差し込み対象
+
+    const createImageHTML = (blob) => {
+      // 画像を表示するためのdiv要素を生成
+      const imageElement = document.createElement('div');
+
+      // 表示する画像を生成
+      const blobImage = document.createElement('img');
+      blobImage.setAttribute('src', blob);
+
+      // 生成したHTMLの要素をブラウザに表示させる
+      imageElement.appendChild(blobImage);
+      ImageList.appendChild(imageElement);
+    };
+
+    document.getElementById('message_image').addEventListener('change', function (e) { // 変化監視要素
+      // 画像が表示されている場合のみ、すでに存在している画像を削除する
+      const imageContent = document.querySelector('img');
+      if (imageContent) {
+        imageContent.remove();
+      }
+
+      const file = e.target.files[0];
+      const blob = window.URL.createObjectURL(file);
+
+      createImageHTML(blob);
+    });
+  }
+});
